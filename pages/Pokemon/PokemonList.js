23d9a77f3c63869d13fd48e712455da368f9b0d0
Kenny <Kennyytran95@gmail.com>
2022-12-20 02:51:05 -0800

    unoptimized filter

--- pages/Pokemon/PokemonList.js
+++ pages/Pokemon/PokemonList.js
@@ -1,33 +1,66 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import PokemonCard from "./PokemonCard";
 import styles from "../../styles/PokemonList.module.css";
 import Link from "next/link";
+import SelectPokemonType from "./SelectPokemonType";
 
 export default function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [loading, toggleLoading] = useState(false);
 
   useEffect(() => {
-    async function getPokemon() {
-      let result = await axios.get("/api/pokedex");
-      let kanto = result.data.slice(0, 386);
-      setPokemonList(kanto);
-    }
     getPokemon();
   }, []);
 
+  async function getPokemon() {
+    toggleLoading(true);
+    let result = await axios.get("/api/pokedex");
+    let kanto = result.data.slice(0, 151);
+    setPokemonList(kanto);
+    toggleLoading(false);
+  }
+
+  async function filterList(type) {
+    console.log(type);
+    if (type === "Show All") {
+      getPokemon();
+    } else {
+      toggleLoading(true);
+      let result = await axios.get("/api/pokedex");
+      let kanto = result.data.slice(0, 151);
+      const filteredPokemon = kanto.filter((pokemon) =>
+        pokemon.types.includes(type.toLowerCase())
+      );
+      toggleLoading(false);
+      setPokemonList(filteredPokemon);
+    }
+  }
+
+  if (loading) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
+
   return (
-    <section className={styles.pokemonList}>
-      {pokemonList.map((pokemonEntry) => {
-        return (
-          <Link
-            href={`/Pokemon/PokemonCard/pokemonentry?id=${pokemonEntry.id}`}
-          >
-            <PokemonCard key={pokemonEntry.id} pokemonEntry={pokemonEntry} />
-          </Link>
-        );
-      })}
-    </section>
+    <>
+      <SelectPokemonType filterList={filterList} />
+      <section className={styles.pokemonList}>
+        {pokemonList.map((pokemonEntry) => {
+          return (
+            <Link
+              href={`/Pokemon/PokemonCard/pokemonentry?id=${pokemonEntry.id}`}
+              key={pokemonEntry.id}
+            >
+              <PokemonCard key={pokemonEntry.id} pokemonEntry={pokemonEntry} />
+            </Link>
+          );
+        })}
+      </section>
+    </>
   );
 }
 
---
+++ pages/Pokemon/SelectPokemonType.js
@@ -0,0 +1,41 @@
+import styles from "../../styles/SelectPokemonType.module.css";
+
+function SelectPokemonType({ filterList }) {
+  return (
+    <div className={styles.selCont}>
+      <div>
+        <label htmlFor="type" className={styles.label}>
+          Filter By Type
+        </label>
+        <br />
+        <select
+          className={styles.selType}
+          id="type"
+          onChange={(e) => filterList(e.target.value)}
+        >
+          <option className={styles.opt} selected disabled hidden>
+            Sort by Type
+          </option>
+          <option className={styles.opt}>Show All</option>
+          <option className={styles.opt}>Normal</option>
+          <option className={styles.opt}>Grass</option>
+          <option className={styles.opt}>Fire</option>
+          <option className={styles.opt}>Water</option>
+          <option className={styles.opt}>Poison</option>
+          <option className={styles.opt}>Electric</option>
+          <option className={styles.opt}>Ground</option>
+          <option className={styles.opt}>Fighting</option>
+          <option className={styles.opt}>Psychic</option>
+          <option className={styles.opt}>Rock</option>
+          <option className={styles.opt}>Ghost</option>
+          <option className={styles.opt}>Dragon</option>
+          <option className={styles.opt}>Dark</option>
+          <option className={styles.opt}>Steel</option>
+          <option className={styles.opt}>Fairy</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default SelectPokemonType;
